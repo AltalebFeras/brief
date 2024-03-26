6042c29b5b290a8a3be01f9c985a21bda749b96c
feras <feras.altalib@gmail.com>
2024-03-26 14:51:31 +0100

    1

---
+++ src/Views/Includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formulaire de réservation Music Vercors Festival</title>
+  <link rel="stylesheet" href="../../../public/assets/style/style.css">
+  <script src="../../../public/assets/scripts/script.js" ></script>
+</head>
+
+    <header>
+        <button id=loginAdmin><a href="/login-admin.php">
+            <img src="assets/logoadmin.png" alt="Logo Admin" id="logoAdmin"> Admin
+        </a></button>
+    </header>
