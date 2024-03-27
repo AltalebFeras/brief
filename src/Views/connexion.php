339c7814be6345f5b3d84aec6e3987ef3d8e2f78
Artchibo7 <arthur.zulpukharov.simplon@gmail.com>
2024-03-27 14:42:00 +0100

    HLILHK

--- src/Views/connexion.php
+++ src/Views/connexion.php
@@ -1,5 +1,12 @@
+<?php
+include_once __DIR__ . '/Includes/header.php';
+?>
+
+
 <form id="formSignIn" method="post" class="
   d-flex flex-column bd-highlight mb-3 form-control" action="treatmentSignIn.php">
+   <fieldset>
+        <h1>Connectez-vous!</h1>
     <div id="message">
         <?php
         if (isset($_SESSION['error_message1'])) {
@@ -13,12 +20,15 @@
         ?>
     </div>
 
-    <label for="email">Email :*</label>
+        <label for="email">Email :*</label>
 
-    <input id="email" type="email" name="email" class="mb-3 mx-2" minlength="3" maxlength="80" placeholder="Enter your email" required>
+        <input id="email" type="email" name="email" class="mb-3 mx-2" minlength="3" maxlength="80" placeholder="Enter your email" required>
 
-    <label for="password">password :*</label>
-    <input id="password" type="password" name="password" class="mb-3 mx-2" minlength="7" placeholder="Enter your password" required>
+        <label for="password">password :*</label>
+        <input id="password" type="password" name="password" class="mb-3 mx-2" minlength="7" placeholder="Enter your password" required>
 
-    <input type="submit" name="submit" value="sign in" class="mb-3 mx-2">
-</form>H
+        <input type="submit" class="btn btn-info" name="submit" value="Se connecter" class="mb-3 mx-2">
+    </fieldset>
+</form>
+
+AA
