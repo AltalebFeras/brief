ec43f8134bc16dfa40bebf2af3a632130b6a86a4
feras <feras.altalib@gmail.com>
2024-03-27 16:03:34 +0100

    Update admin.php

--- src/Views/admin.php
+++ src/Views/admin.php
@@ -27,3 +27,4 @@ include_once __DIR__ . '/Includes/header.php';
             <img src="assets/logoadmin.png" alt="Logo Admin" id="logoAdmin"> Admin
         </a></button>
     </header> -->
+    JQSJDDLJQ
